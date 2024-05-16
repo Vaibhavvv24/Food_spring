07d2ded18576666977b37a0d5e9dd725719ae0be
Vaibhav <mittalvaibhav277@gmail.com>
2024-05-16 13:57:30 +0530

    finsihed frontend of showing order history

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,5 +1,23 @@
 import React, { useEffect, useState } from "react";
 
+function convertDate(date) {
+  const timestamp = date;
+  const dateObject = new Date(timestamp);
+
+  // Convert to IST
+  dateObject.setHours(dateObject.getHours()); // Add 5 hours for IST
+  dateObject.setMinutes(dateObject.getMinutes()); // Add 30 minutes for IST
+
+  const year = dateObject.getFullYear();
+  const month = dateObject.getMonth() + 1; // Note: getMonth() returns zero-based month (0 for January)
+  const day = dateObject.getDate();
+
+  const hours = dateObject.getHours();
+  const minutes = dateObject.getMinutes();
+  const seconds = dateObject.getSeconds();
+  return `Date:${day}-${month}-${year}
+   Time: ${hours}:${minutes}:${seconds}`;
+}
 const Orders = () => {
   const [orders, setOrders] = useState([]);
 
@@ -24,15 +42,15 @@ const Orders = () => {
   return (
     <div>
       <h1>Orders</h1>
-      {/* {orders.map((item) => (
+      {orders.map((item) => (
         <div className="flex flex-col" key={item.id}>
-          <p>{item.restrauntName}</p>
-          <p>{item.}</p>
-          <p>{item.restrauntName}</p>
-          <Base64decode base64String={item.productImg} />
-          <button onClick={() => handleRemove(item.id)}>Remove</button>
+          <p>{item.orderId}</p>
+          <p>{item.orderStatus}</p>
+          <p>{item.restName}</p>
+          <p>{item.ownerName}</p>
+          <p>{convertDate(item.orderedAt)}</p>
         </div>
-      ))} */}
+      ))}
     </div>
   );
 };
