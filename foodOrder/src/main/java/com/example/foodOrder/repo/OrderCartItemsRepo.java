070bc6557a4e40b65220f3dc50983ef32f681094
Vaibhav <mittalvaibhav277@gmail.com>
2024-05-16 18:34:34 +0530

    deleted it

--- foodOrder/src/main/java/com/example/foodOrder/repo/OrderCartItemsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.foodOrder.repo;
-
-import com.example.foodOrder.entity.OrderCartItems;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderCartItemsRepo extends JpaRepository<OrderCartItems,Long> {
-}
